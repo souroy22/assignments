eaf4ca52b7464cb50a792ff01fbddf03383b699d
codecreatorsss <codecreatorsss@gmail.com>
2023-12-09 20:50:33 +0530

    Setting up project

--- node_modules/openai-sdk/dist/lib/API.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const CHAT = "https://openai.weixin.qq.com/openapi/message";
-export declare const TOKENIZE = "https://openai.weixin.qq.com/openapi/nlp/tokenize";
-export declare const NER = "https://openai.weixin.qq.com/openapi/nlp/ner";
-export declare const SENTIMENT = "https://openai.weixin.qq.com/openapi/nlp/sentiment";
-export declare const SENSITIVE = "https://openai.weixin.qq.com/openapi/nlp/sensitive";
--- node_modules/openai-sdk/dist/lib/API.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CHAT = 'https://openai.weixin.qq.com/openapi/message';
-exports.TOKENIZE = 'https://openai.weixin.qq.com/openapi/nlp/tokenize';
-exports.NER = 'https://openai.weixin.qq.com/openapi/nlp/ner';
-exports.SENTIMENT = 'https://openai.weixin.qq.com/openapi/nlp/sentiment';
-exports.SENSITIVE = 'https://openai.weixin.qq.com/openapi/nlp/sensitive';
